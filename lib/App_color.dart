dc5626b65fd1fa4519b79c0c4efa09430b24ff10
soul071213 <js071213asd@gmail.com>
2024-12-12 10:45:28 +0900

    기초 세팅 완료

---
+++ lib/App_color.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color gray50 = Color(0xffF9FAFB);
+  static Color gray100 = Color(0xfff3f4f6);
+  static Color gray200 = Color(0xffe5e7eb);
+  static Color gray300 = Color(0xffd1d5db);
+  static Color gray400 = Color(0xff9ca3af);
+  static Color gray500 = Color(0xff6b7280);
+  static Color gray600 = Color(0xff4b5563);
+  static Color gray700 = Color(0xff374151);
+  static Color gray800 = Color(0xff1f2937);
+  static Color gray900 = Color(0xff111827);
+  static Color gray950 = Color(0xff030712);
+
+
+
+  static Color yellow50 = Color(0xfffefce8);
+  static Color yellow100 = Color(0xfffef9c3);
+  static Color yellow200 = Color(0xfffef08a);
+  static Color yellow300 = Color(0xfffde047);
+  static Color yellow400 = Color(0xfffacc15);
+}
---
+++ lib/App_font.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppFont {
+  static TextStyle MinLight = TextStyle(
+      fontFamily: 'MinLight',
+    );
+
+  static TextStyle Minregular = TextStyle(
+      fontFamily: 'MinRegular',
+    );
+  static TextStyle Minmedium= TextStyle(
+      fontFamily: 'MinMedium');
+
+  static TextStyle MinsemiBold = TextStyle(
+      fontFamily: 'MinSemiBold',
+      );
+
+  static TextStyle Minbold = TextStyle(
+      fontFamily: 'MinBold');
+
+}
